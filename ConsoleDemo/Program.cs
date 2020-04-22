6da8372550d6e807e145d49df683e17d171eec9d
Derek Ji <syfool@gmail.com>
2020-04-22 23:04:03 +0930

    Initial commit

---
+++ ConsoleDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+using System;
+
+namespace ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testDB();
+        }
+
+        static void testDB()
+        {
+            using (var db = new NorthwindContext())
+            {
+                foreach (var prod in db.Products)
+                {
+                    Console.WriteLine(" - {0}", prod.ProductName);
+                }
+            }
+        }
+    }
+}
---
+++ Utils/Extensions/QueryableExtentions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Utils.Extensions
+{
+    public static class QueryableExtentions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> 
+            WhereIf<TSource>
+            (this IEnumerable<TSource> source,
+            bool condition,
+            Func<TSource, bool> predicate)
+        {
+            return condition
+                ? source.Where(predicate)
+                : source;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TProperty"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="condition"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static IQueryable<T> 
+            IncludeIf<T, TProperty>
+            (this IQueryable<T> source, 
+            bool condition, 
+            Expression<Func<T, TProperty>> path)
+            where T : class
+        {
+            return condition
+                ? source.Include(path)
+                : source;
+        }
+    }
+}
