7b10860340ba292695a07ed9a4a5ab8ef96fddf1
Derek Ji <syfool@gmail.com>
2020-05-02 08:46:09 +0930

    Configure Swagger

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -10,11 +10,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace WebApi
 {
     public class Startup
     {
+        private static string apiVersion = "v1";
+        private static string apiTitle = $"Demo Web API";
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,11 +31,27 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(apiVersion, new OpenApiInfo { Title = apiTitle, Version = apiVersion });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json", apiTitle);
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
